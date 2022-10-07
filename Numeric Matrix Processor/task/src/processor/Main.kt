958ab8b7753b9a35b668a6e11ef22b92fd1ca5ed
BRDLF <afassl34@outlook.com>
2022-10-07 14:43:23 -0500

    Finished Stage 5/6

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -12,6 +12,7 @@ object MatrixProcessor {
                     "2" -> matrixMultiplyByConstant()
                     "3" -> matrixMultiplyByMatrix()
                     "4" -> matrixTranspose()
+                    "5" -> matrixDeterminant()
                     "0" -> break
                 }
                 println()
@@ -27,6 +28,7 @@ object MatrixProcessor {
         2. Multiply matrix by a constant
         3. Multiply matrices
         4. Transpose matrix
+        5. Calculate a determinant
         0. Exit
         Your choice: 
     """.trimIndent())
@@ -35,6 +37,38 @@ object MatrixProcessor {
         val size = readln().split(" ").map {it.toIntOrNull()?: throw MatrixProcessorException("$it is not an int")}.take(2)
         println("Enter$num matrix:")
         return List(size[0]) { readln().split(" ").map { it.toDoubleOrNull()?: throw MatrixProcessorException("$it is not a float") }.take(size[1]) }
+    }
+    private fun matrixDeterminant() {
+        val m1 = matrixInput()
+        val memo: MutableMap<List<List<Double>>, Double> = mutableMapOf<List<List<Double>>, Double>()
+        fun recursiveDeterminant(passed: List<List<Double>>, subIndex: Int = 0): Double {
+            if (passed.size != passed.first().size) throw MatrixProcessorException("Cannot find determinant of non-square matrix")
+            if (passed.size == 2) {
+                return passed[0][0]*passed[1][1] - passed[1][0]*passed[0][1]
+            }
+            var returnValue = 0.0
+            for (index in passed[0].indices) {
+                val cofactor = passed[0][index]
+                if (passed.size < m1.size-1) { //prevent memo-ing first-level submatrix of given matrix
+                    val newList = passed.filterIndexed { row, _ -> row != 0 }.map{ it.filterIndexed{ col, _ -> col != index}}
+                    if (memo[newList]==null) {
+                        memo[newList] = recursiveDeterminant(newList)
+                    }
+                    returnValue = if (index%2 == 0) {
+                        returnValue + cofactor*memo[newList]!!
+                    } else returnValue - cofactor*memo[newList]!!
+                }
+                else {
+                    val newList = passed.filterIndexed { row, _ -> row != 0 }.map{ it.filterIndexed{ col, _ -> col != index}}
+                    returnValue = if (index%2 == 0) {
+                        returnValue + cofactor*recursiveDeterminant(newList)
+                    } else returnValue - cofactor*recursiveDeterminant(newList)
+                }
+            }
+            return returnValue
+        }
+
+        println("The result is:\n${recursiveDeterminant(m1)}")
     }
     private fun matrixMultiplyByConstant() {
         val m1 = matrixInput()
